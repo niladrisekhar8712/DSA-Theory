907a352f282671ea9b995137104234126dd511fc
Niladri Sekhar Mondal <115944239+niladrisekhar8712@users.noreply.github.com>
2024-01-13 20:33:47 +0530

    only tabulation

--- JavaPractice/DP 24 Rod Cutting Problem/src/Main.java
+++ JavaPractice/DP 24 Rod Cutting Problem/src/Main.java
@@ -4,19 +4,19 @@ public class Main {
     }
     public static int cutRod(int[] price, int n) {
         int[][] dp = new int[][]
-        return recurr(price, n, n-1);
+        return f(price);
     }
-    private static int recurr(int[] price, int n, int ind){
-        if(ind == 0){
-            return n*price[ind];
-        }
-        int notPick = recurr(price, n, ind-1);
-        int pick = 0;
-        if(ind+1<=n){
-            pick = price[ind] + recurr(price, n-ind-1, ind);
-        }
-        return Math.max(notPick,pick);
-    }
+    // private static int recurr(int[] price, int n, int ind){
+    //     if(ind == 0){
+    //         return n*price[ind];
+    //     }
+    //     int notPick = recurr(price, n, ind-1);
+    //     int pick = 0;
+    //     if(ind+1<=n){
+    //         pick = price[ind] + recurr(price, n-ind-1, ind);
+    //     }
+    //     return Math.max(notPick,pick);
+    // }
     public static int f(int[] arr){ // tabulation approach
         int[][] dp = new int[arr.length][arr.length+1];
         for(int i = 0;i<dp.length;i++){
